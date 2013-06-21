a03d00ace8a25c300f36c2a8886227d9e4c8f800
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2013-06-21 20:40:56 +0200

    feature: add LICENCE file and setup metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,13 @@
 name             'opentsdb'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+license          'Apache v2.0'
 description      'Installs/Configures opentsdb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+maintainer       "Christophe Furmaniak"
+maintainer_email "christophe.furmaniak@gmail.com"
+depends			 "yum"
+recipe 			 "opentsdb", "Installs HBase and OpenTSDB from source"
 
+%w{ centos }.each do |os|
+  supports os
+end
