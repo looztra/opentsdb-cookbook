f068d55cba1e14efd703a2b7b3f2ace7692300eb
Christophe Furmaniak <christophe.furmaniak@atos.net>
2014-06-23 19:26:46 +0200

    feature: refactoring and tests to get every started as expected

--- recipes/init_opentsdb.rb
+++ recipes/init_opentsdb.rb
@@ -1,30 +1,35 @@
+# include Chef::Mixin::Shellout
+::Chef::Recipe.send(:include, Chef::Mixin::ShellOut)
 
 log 'Creating OpenTSDB HBase tables if needed'
-log "executing command: ps auxwww | grep 'org.apache.hadoop.hbase.master.HMaster start' | grep -v grep"
-if system("ps auxwww | grep 'org.apache.hadoop.hbase.master.HMaster start' | grep -v grep")
-	log "HBASE seems started"
+
+if shell_out("ps auxwww | grep 'org.apache.hadoop.hbase.master.HMaster start' | grep -v grep | wc -l").stdout.chomp == "1"
+	hbase_is_up=true
+	log "(init_opentsdb) HBASE seems started"
 else
-	log "HBASE does not seem started"
+	hbase_is_up=false
+	log "(init_opentsdb) HBASE does not seem started, we won't try to create the tables"
 end
 
-log "executing command: test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb && test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb-uid"
-if system("test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb && test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb-uid")
-	log "(before) tsdb HBASE tables does not exist"
+if shell_out("test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb && test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb-uid").exitstatus == 0
+	tsdb_hbase_tables_exist=true
+	log "(before) tsdb HBASE tables exist, we won't try to create them"
 else
-	log "(before) tsdb HBASE tables exist"
+	tsdb_hbase_tables_exist=false
+	log "(before) tsdb HBASE tables do not exist, we should create them"
 end
 
 
 execute "create OpenTSDB hbase tables" do
 	cwd "#{node['opentsdb']['tsdb_installdir']}/opentsdb"
-	command "./src/create_table.sh"
-	only_if "ps auxwww | grep 'org.apache.hadoop.hbase.master.HMaster start' | grep -v grep"
-	not_if "test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb && test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb-uid"	
+	command "./src/create_table.sh >> /var/log/hbase.create_tsdb_tables.log 2>&1"
+	only_if { hbase_is_up }
+	not_if  { tsdb_hbase_tables_exist }
 	environment ({'HBASE_HOME' => "#{node['opentsdb']['hbase_installdir']}/hbase", "COMPRESSION" => "none"})
 end
 
-if system("test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb && test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb-uid")
-	log "(after) tsdb HBASE tables does not exist"
+if shell_out("test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb && test -d #{node['opentsdb']['hbase_rootdir']}/hbase-root/hbase/tsdb-uid").exitstatus == 0
+	log "(after) tsdb HBASE tables exist, w00t"
 else
-	log "(after) tsdb HBASE tables exist"
+	log "(after) tsdb HBASE tables do not exist, check logs"
 end
--- recipes/start_hbase.rb
+++ recipes/start_hbase.rb
@@ -1,5 +1,17 @@
+# include Chef::Mixin::Shellout
+::Chef::Recipe.send(:include, Chef::Mixin::ShellOut)
+
+
+if shell_out("ps auxwww | grep 'org.apache.hadoop.hbase.master.HMaster start' | grep -v grep | wc -l").stdout.chomp == "1"
+	hbase_is_up=true
+	log "(start_hbase) HBASE seems started"
+else
+	hbase_is_up=false
+	log "(start_hbase) HBASE does not seem started, we will start it"
+end
+
 execute "start hbase if needed" do
 	command "#{node['opentsdb']['hbase_installdir']}/hbase/bin/start-hbase.sh" 
-	not_if "ps auxwww | grep 'org.apache.hadoop.hbase.master.HMaster start' | grep -v grep"
+	not_if { hbase_is_up }
 	environment ({'HBASE_HOME' => "#{node['opentsdb']['hbase_installdir']}/hbase"})
 end
